4931a77d45819f33c61d453408bae2f3957b4963
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-06-29 17:59:34 -0300

    style

--- tutorial/sql-databases/sql_app/crud.py
+++ tutorial/sql-databases/sql_app/crud.py
@@ -6,11 +6,17 @@ from sqlalchemy.sql.functions import mode, user
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
+def get_user(
+    db: Session, 
+    user_id: int
+):
     return db.query(models.User).filter(models.User.id == user_id).first()
     
 
-def remove_user(db: Session, user_id: int):
+def remove_user(
+    db: Session, 
+    user_id: int
+):
     db_user = db.query(models.User).get(user_id)
     db.delete(db_user)
     db.commit()
@@ -53,14 +59,25 @@ def update_user(
     return db_user
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(
+    db: Session, 
+    email: str
+):
     return db.query(models.User).filter(models.User.email == email).first()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
+def get_users(
+    db: Session, 
+    skip: int = 0, 
+    limit: int = 100
+):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-def create_user(db: Session, user: schemas.UserCreate):
+
+def create_user(
+    db: Session, 
+    user: schemas.UserCreate
+):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
@@ -69,11 +86,17 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-def get_item(db: Session, item_id: int):
+def get_item(
+    db: Session,
+    item_id: int
+):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
     
 
-def remove_item(db: Session, item_id: int):
+def remove_item(
+    db: Session, 
+    item_id: int
+):
     db_item = db.query(models.Item).get(item_id)
     db.delete(db_item)
     db.commit()
@@ -109,11 +132,19 @@ def update_item(
     return db_item
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
+def get_items(
+    db: Session, 
+    skip: int = 0, 
+    limit: int = 100
+):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+def create_user_item(
+    db: Session, 
+    item: schemas.ItemCreate, 
+    user_id: int
+):
     db_item = models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
